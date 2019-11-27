b992817b3211bd6a6fe6149b27d315731dc97b03
shadow-fiend001 <shadow.fiend@aliyun.com>
2019-11-27 09:37:42 +0800

    update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  jsonApiRootUrl: 'https://polkascan.io/kulupu/api/v1/',
+  jsonApiRootUrl: 'http://127.0.0.1:8001/',
   networkName: 'YeeCo',
   networkTokenSymbol: 'YEE',
   networkTokenDecimals: 12
