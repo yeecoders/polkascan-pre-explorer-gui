2ee98fd5af77653fdd217edec836c1978a805e9a
shadow-fiend001 <shadow.fiend@aliyun.com>
2020-03-25 12:25:15 +0800

    add get target

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -35,6 +35,10 @@ import {environment} from '../../../environments/environment';
 import {Jsonrpc} from '../wallet-detail/jsonrpc.class';
 import {Data} from '../dashboard/data.class';
 import {handleHashRate, handleDifficulty} from '../dashboard/number';
+import {
+  encode, decode, calls, runtime, chain, system, runtimeUp, pretty,
+  addressBook, secretStore, metadata, nodeService, bytesToHex, AccountId, hexToBytes, TransactionEra, StorageBond
+} from 'oo7-substrate';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -67,6 +71,8 @@ export class DashboardComponent implements OnInit, OnDestroy {
   // 1 THash/s = 1000 GHash/s
   // 1 PHash/s = 1000 THash/s
   async ngOnInit() {
+    // tslint:disable-next-line:max-line-length
+    await this.get_target('0x0459656521750350907ff82b2d1fb8d7d07fe9cc0c6e33454e1e98abce6714d20567fbf33be078eee6e993fca3bd8c7a31aaee6520b968afc1f941c88572e2d65b1a4f1f010000bae45f0f7101000002287965652d7377697463686c723bd5a554413fcaa3272fc5c68be002ae10bd837f43a37f6f7412a582dd6a252d00000000000008e6fc24bc1df1907dae7c4407d16cbc022e94210a581e119e22be83ac09ccf016a93f20da95d9a3e1a3c5ec5bd8ec24279ee9f2925d4f8b71107b46389961bde885c09af929492a871e4fae32d9d5c36e352471cd659bcdb61de08f1722acc3b1');
     // const model1 = new Data('0', '', '');
     // const model2 = new Data('1', '', '');
     // const model3 = new Data('2', '', '');
@@ -98,14 +104,34 @@ export class DashboardComponent implements OnInit, OnDestroy {
       this.getBlocks();
     });
   }
-  getModel(i: any){
+  get_target(input: string) {
+      console.log(' target: ', input);
+      input = input.replace('0x', '');
+      const digestItemType = decode(hexToBytes(input.substr(0, 2)), 'u16');
+      if (digestItemType !== 4) { // consensus
+        return null;
+      }
+      // 81db690f71010000  1585101527937
+      console.log('input---', input.substr(78, 64));
+      return new Promise((resolve, reject) => {
+        const time = decode(hexToBytes('81db690f71010000', 'u64'))
+        resolve(time);
+      });
+        // const timestamp = decode(hexToBytes('81db690f71010000', 'u64'));
+      // const timestamp1 = decode(hexToBytes('81db690f', 'u32'));
+      // const timestamp2 = decode(hexToBytes('71010000', 'u32'));
+      // console.log('input---timestamp', timestamp);
+      // console.log('input---timestamp1', timestamp1);
+      // console.log('input---timestamp2', timestamp2);
+      // resolve(timestamp);
+  }
+  getModel(i: any) {
     return new Promise((resolve, reject) => {
       const model = new Data(i.toString(), '', '');
       const headers = new HttpHeaders().set('Content-Type', 'application/json');
       // tslint:disable-next-line:max-line-length
       this.http.post(
         'http://18.138.185.139:9933/',
-        // tslint:disable-next-line:max-line-length
         {
           jsonrpc: '2.0' ,
           method: 'mining_getJob',
@@ -115,7 +141,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
         // tslint:disable-next-line:no-eval
         if (res.result === undefined ) {
           console.log('mining_getJob: ',  res.error);
-          reject(res.error)
+          reject(res.error);
         } else {
           console.log(' mining_getJob: ', res.result.digest_item.pow_target);
           const str = res.result.digest_item.pow_target;
@@ -128,10 +154,9 @@ export class DashboardComponent implements OnInit, OnDestroy {
           model.rate = handleHashRate(hashrate);
           model.difficulty = handleDifficulty(diff);
           resolve(model);
-          // return model;
         }
       });
-    })
+    });
   }
   getBlocks(): void {
     this.blockService.all({
