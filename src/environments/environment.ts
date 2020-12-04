70cd8b47f90e5b0fd5846583523b81e2d6416ee7
Zhao Zhao <2655919360@qq.com>
2020-12-04 15:57:22 +0800

    测试

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,18 @@
+// export const environment = {
+//   production: false,
+//   jsonApiRootUrl: 'https://testnet.yeescan.org/api/v1/',
+//   networkName: 'YeeCo',
+//   networkTokenSymbol: 'YEE',
+//   HRP: 'tyee',
+//   networkTokenDecimals: 8,
+//   switchRootUrl: 'https://testnet.yeescan.org/switch/api/',
+// };
 export const environment = {
   production: false,
-  jsonApiRootUrl: 'https://testnet.yeescan.org/api/v1/',
-  networkName: 'YeeCo',
-  networkTokenSymbol: 'YEE',
-  HRP: 'yee',
+  jsonApiRootUrl: "https://yeescan.org/api/v1/", //
+  networkName: "YeeCo",
+  networkTokenSymbol: "YEE",
+  HRP: "yee",
   networkTokenDecimals: 8,
-  switchRootUrl: 'https://testnet.yeescan.org/switch/api/',
+  switchRootUrl: "https://yeescan.org/switch/api/",
 };
